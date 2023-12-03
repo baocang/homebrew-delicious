7e3132d9b13f940e41b1ca91a367c403ceb16be7
Baocang Nie <baocang.nie@icloud.com>
2023-12-03 20:12:46 +0800

    Add sshpass from sourceforge.net

---
+++ Formula/sshpass.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Sshpass < Formula
+  url 'https://sourceforge.net/projects/sshpass/files/sshpass/1.10/sshpass-1.10.tar.gz/download'
+  homepage 'http://sourceforge.net/projects/sshpass'
+  sha256 'ad1106c203cbb56185ca3bad8c6ccafca3b4064696194da879f81c8d7bdfeeda'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+
+  def test
+    system "sshpass"
+  end
+end
